5bdfe2bd0f277682393efa6a543838bb0c5b5531
Ash Fallon <ashfallon23@gmail.com>
2022-06-10 22:24:47 +0100

    Added initial styling to navbar, added initial global styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,12 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-20 flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       {/* Logo */}
-      <div></div>
+      <div className='text-2xl'>{`<Ash Fallon />`}</div>
 
       {/* Menu */}
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* Hamburger menu */}
-      <div className='hidden'>
-        <FaBars />
-      </div>
-
-      {/* Mobile menu */}
-      <ul className='hidden'>
+      <ul className='hidden md:flex'>
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -31,8 +15,22 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
 
+      {/* Hamburger menu */}
+      <div className='md:hidden z-10'>
+        <FaBars />
+      </div>
+
+      {/* Mobile menu */}
+      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
+      </ul>
+
       {/* Social Icons */}
-      <div></div>
+      <div className='hidden'></div>
     </div>
   );
 };
