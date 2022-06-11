c9ff1d995993912cbf83c34e3e74e0e45e72e717
Ash Fallon <ashfallon23@gmail.com>
2022-06-11 03:12:26 +0100

    Added smooth scrolling and some links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -9,8 +10,18 @@ const Navbar = () => {
 
   const socials = [
     { name: 'LinkedIn', icon: FaLinkedin, url: '/', colour: 'bg-blue-700' },
-    { name: 'Github', icon: FaGithub, url: '/', colour: 'bg-[#333333]' },
-    { name: 'Email', icon: HiOutlineMail, url: '/', colour: 'bg-[#6fc2b0]' },
+    {
+      name: 'Github',
+      icon: FaGithub,
+      url: 'https://github.com/ash-fallon',
+      colour: 'bg-[#333333]',
+    },
+    {
+      name: 'Email',
+      icon: HiOutlineMail,
+      url: 'mailto:ash@ashfallon.com',
+      colour: 'bg-[#6fc2b0]',
+    },
     {
       name: 'CV',
       icon: BsFillPersonLinesFill,
@@ -28,6 +39,8 @@ const Navbar = () => {
         <a
           className='flex justify-between items-center w-full text-gray-300'
           href={social.url}
+          target='_blank'
+          rel='noreferrer'
         >
           {social.name} <social.icon size={30} />
         </a>
@@ -42,11 +55,31 @@ const Navbar = () => {
 
       {/* Menu */}
       <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='projects' smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Hamburger menu */}
@@ -63,11 +96,41 @@ const Navbar = () => {
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={clickHandler} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={clickHandler} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={clickHandler} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={clickHandler}
+            to='projects'
+            smooth={true}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={clickHandler}
+            to='contact'
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
